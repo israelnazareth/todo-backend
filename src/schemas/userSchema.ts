6dbb3cb27908d68d9e2f265788363e9ff7d4aa7f
Israel Nazareth <israelsnazareth@gmail.com>
2025-03-02 01:52:16 -0300

    feat: create user schema

---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,7 @@
+import { z } from 'zod';
+
+export const userSchema = z.object({
+  name: z.string().min(3, "Nome deve ter pelo menos 3 caracteres"),
+  email: z.string().email("E-mail inválido"),
+  password: z.string().min(6, "Senha deve ter pelo menos 6 caracteres"),
+});
